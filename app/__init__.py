df0bb79f181aae98ceabd2b4ada480b4aeb211e5
quicksilver32 <ameliya9@mail.ru>
2018-12-01 12:22:14 +0300

    get method

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'hello'
+
+
+@app.route('/news', methods = ['GET'])
+def get_news():
+    return 'hi'
+
+
